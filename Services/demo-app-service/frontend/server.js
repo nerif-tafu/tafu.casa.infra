024194da44beddd69f0c3af591bbc399b2d5dd19
nerif-tafu <finn.rawles@hotmail.co.uk>
2025-03-20 14:31:13 -0300

    Got app demo api working ish, need to fix frontend.

--- Services/demo-app-service/frontend/server.js
+++ Services/demo-app-service/frontend/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 9000;
 
 app.get('/', (req, res) => {
-  res.send('Frontend Server Running');
+  res.send('Frontend Server Running test');
 });
 
 app.listen(port, () => {
